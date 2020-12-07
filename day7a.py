dfe103b551a09a09468410786f6d935ff53b8ba7
Wouter <wouter075@gmail.com>
2020-12-07 23:01:00 +0100

    Still not working 7

--- day7a.py
+++ day7a.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 with open('day7.txt') as f:
     lines = [line.rstrip() for line in f]
 
@@ -26,56 +28,60 @@ for line in lines:
         contains = []
 
         bag_color, bag_contains_number, bag_contains_color = extract_bag(parts[0])
-        all_bags[bag_color] = {0: {
-            'color': bag_contains_color,
-            'count': bag_contains_number
-        }}
+        # all_bags[bag_color] = {0: {
+        #     'color': bag_contains_color,
+        #     'count': bag_contains_number
+        # }}
+        all_bags[bag_color] = []
+        # all_bags[bag_color].extend(bag_contains_color)
+        all_bags[bag_color].append(bag_contains_color)
 
         for x in range(1, len(parts)):
             d = parts[x].split(" ")
             num = int(d[0])
             color = f"{d[1]} {d[2]}".replace(".", "")
 
-            all_bags[bag_color].update({x: {
-                'color': color,
-                'count': num
-            }})
-
+            # all_bags[bag_color].update({x: {
+            #     'color': color,
+            #     'count': num
+            # }})
+            # print(color)
+            all_bags[bag_color].append(color)
+        # print(contains)
+        # all_bags[bag_color] = contains
     else:
         # single
         # striped olive bags contain 4 dark crimson bags.
         bag_color, bag_contains_number, bag_contains_color = extract_bag(line)
-        all_bags[bag_color] = {0: {
-            'color': bag_contains_color,
-            'count': bag_contains_number
-        }}
+        # all_bags[bag_color] = {0: {
+        #     'color': bag_contains_color,
+        #     'count': bag_contains_number
+        # }}
+        all_bags[bag_color] = []
+        # all_bags[bag_color].append(bag_contains_color)
 
-# print(all_bags)
-for a in all_bags:
-    print(a)
-    print(all_bags[a])
-    print("")
+print(all_bags)
 
+# http://kmkeen.com/python-trees/
 bag_list = []
-count = 0
 
-string = ""
+def children(token, tree):
+    "returns a list of every child"
+    print("X")
+    visited = set()
+    to_crawl = deque([token])
+    while to_crawl:
+        current = to_crawl.popleft()
+        if current in visited:
+            continue
+        visited.add(current)
+        node_children = set(tree[current])
+        to_crawl.extend(node_children - visited)
+    return list(visited)
 
 
-def follow(bags):
-    print(bags)
-    for b in bags.keys():
-        color = bags[b]['color']
-        if bags[b]['count'] > 0:
-            # string += f"{}"
-            bag_list.append(color)
-            # count += 1
-            follow(all_bags[color])
+print(children('shiny gold', all_bags))
 
 
-follow(all_bags['shiny gold'])
-print(count)
-print("-"*20)
-print(len(bag_list))
-print(bag_list)
+
 # first: 49
